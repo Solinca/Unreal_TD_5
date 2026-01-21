c8a7f5db4e1a104c44b61453ad9632adee41f4e4
Nicolas Corbi <n.corbi@GA.local>
2026-01-21 13:37:54 +0100

    Init project with Player, Controller and inputs

---
+++ Source/TD_5_Nicolas_Corbi/Private/Player/MyPlayerController.cpp
@@ -0,0 +1,89 @@
+#include "Player/MyPlayerController.h"
+#include "Player/MyCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+AMyPlayerController::AMyPlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!MappingContext)
+	{
+		return;
+	}
+
+	if (ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(Player))
+	{
+		if (TObjectPtr<UEnhancedInputLocalPlayerSubsystem> EILPS = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			EILPS->AddMappingContext(MappingContext, 0);
+		}
+	}
+
+	MyChara = Cast<AMyCharacter>(GetPawn());
+
+	DefaultMaxSpeed = MyChara->GetCharacterMovement()->MaxWalkSpeed;
+
+	PlayerCameraManager->ViewPitchMin = -20;
+
+	PlayerCameraManager->ViewPitchMax = 20;
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (TObjectPtr<UEnhancedInputComponent> EIC = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		for (FInputData data : InputDataList)
+		{
+			EIC->BindAction(data.Action, data.Event, this, data.ActionName.GetMemberName());
+		}
+	}
+}
+
+void AMyPlayerController::Move(const FInputActionValue& Value)
+{
+	FVector Movement = Value.Get<FVector>();
+
+	FRotator CameraRotation = FRotator(0, GetControlRotation().Yaw, 0);
+
+	MyChara->AddMovementInput(CameraRotation.RotateVector(Movement));
+}
+
+void AMyPlayerController::Look(const FInputActionValue& Value)
+{
+	FVector2D Values = Value.Get<FVector2D>();
+
+	AddYawInput(Values.X * CameraSensitivity * GetWorld()->GetDeltaSeconds());
+
+	AddPitchInput(Values.Y * CameraSensitivity * GetWorld()->GetDeltaSeconds());
+}
+
+void AMyPlayerController::Jump(const FInputActionValue& Value)
+{
+	MyChara->Jump();
+}
+
+void AMyPlayerController::SprintStart(const FInputActionValue& Value)
+{
+	MyChara->GetCharacterMovement()->MaxWalkSpeed = DefaultMaxSpeed * PlayerSprintFactor;
+}
+
+void AMyPlayerController::SprintEnd(const FInputActionValue& Value)
+{
+	MyChara->GetCharacterMovement()->MaxWalkSpeed = DefaultMaxSpeed;
+}
+
+void AMyPlayerController::InteractStart(const FInputActionValue& Value)
+{
+}
+
+void AMyPlayerController::InteractEnd(const FInputActionValue& Value)
+{
+}
