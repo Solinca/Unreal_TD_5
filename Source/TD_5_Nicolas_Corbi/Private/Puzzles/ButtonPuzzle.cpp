bd493590f666583d7c87101f2f07772e11a0ff88
Nicolas Corbi <n.corbi@GA.local>
2026-01-27 15:16:15 +0100

    Started second puzzle with staircase and button

---
+++ Source/TD_5_Nicolas_Corbi/Private/Puzzles/ButtonPuzzle.cpp
@@ -0,0 +1,1 @@
+#include "Puzzles/ButtonPuzzle.h"
---
+++ Source/TD_5_Nicolas_Corbi/Public/Puzzles/ButtonPuzzle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Puzzles/BasePuzzle.h"
+#include "ButtonPuzzle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TD_5_NICOLAS_CORBI_API AButtonPuzzle : public ABasePuzzle
+{
+	GENERATED_BODY()
+	
+};
